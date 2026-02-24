60aab43e04d084938d09b65ef62b0ffa1f22b445
alhasani home center car <laethazherx1@gmail.com>
2026-02-24 20:42:01 +0300

    Fix port for Render

--- server.ts
+++ server.ts
@@ -51,7 +51,7 @@ function safeParse(v: any) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: '50mb' }));
 
