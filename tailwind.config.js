45fa951f1756e332f3e7907700c86e144d2a67f5
alhasani home center car <laethazherx1@gmail.com>
2026-02-25 23:51:07 +0300

    fix dark mode configuration with tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        stone: {
+          50: '#fafaf9',
+          100: '#f5f5f4',
+          200: '#e7e5e4',
+          300: '#d6d3d1',
+          400: '#a8a29e',
+          500: '#78716c',
+          600: '#57534e',
+          700: '#44403c',
+          800: '#292524',
+          900: '#1c1917',
+          950: '#0f0e0d',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
